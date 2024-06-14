70c432a176e02f930f9c38f30c92d66296224eda
Kristian Gavalier <Kristian.Gavalier@icloud.com>
2024-06-14 11:04:12 +0200

    SPKG-13-1

---
+++ src/main/java/de/pdv/demo/carrentals/api/CarController.java
@@ -0,0 +1,44 @@
+package de.pdv.demo.carrentals.api;
+
+import java.util.Optional;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import de.pdv.demo.carrentals.model.Car;
+import de.pdv.demo.service.carService;
+
+
+@RestController
+@RequestMapping("api/cars/{id}")
+public class CarController {
+
+    @GetMapping("api/cars")
+    public Iterable<Car> getAllCars() {
+        return carService.getAllCars();
+    }
+
+    @GetMapping("api/cars/{id}")
+    public Optional<Car> getCarById() {
+        return carService.getCarById(); 
+    }
+
+    @PostMapping("api/cars")
+    public Car saveCar() {
+        return carService.saveCar(); 
+    }
+
+    @PutMapping("api/cars/{id}")
+    public () {
+        return;
+    }
+
+    @DeleteMapping("api/cars/{id}")
+    public boolean deleteCar() {
+        return carService.deleteCar(); 
+    }
+
+}
---
+++ src/main/java/de/pdv/demo/carrentals/service/carService.java
@@ -0,0 +1,25 @@
+package de.pdv.demo.carrentals.service;
+
+import org.springframework.stereotype.Component;
+
+
+@Component
+public class carService {
+
+    public String getAllCars() {
+        return cars
+    }
+
+    public String getCarById(Long id) {
+        return 
+    }
+
+    public String saveCar(Car car) {
+        return 
+    }
+
+    public String deleteCar(Long id) {
+        return
+    }
+
+}
